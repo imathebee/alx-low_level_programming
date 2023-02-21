7a3c6cef4a9ed12dd2955fea31802b121b494582
Imane O <ouiyzmeimane@gmail.com>
2023-02-21 12:10:10 -0800

    added f1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-/**
- * print_alphabet - prints the alphabet in lowercase
- *
- * Return: void
- */
+#include <unistd.h>
 /**
  * _putchar - writes a character to the standard output
- * @c: the character to write
  *
- * Return: on success, the character written
- * On error, -1 is returned, and errno is set appropriately.
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
-return putchar(c);
+return (write(1, &c, 1));
 }
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 char ch = 'a';
