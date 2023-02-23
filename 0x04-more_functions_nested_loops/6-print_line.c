dde8644e3eddb285583746f87a1c1d00cd2efeda
Imane O <ouiyzmeimane@gmail.com>
2023-02-23 14:33:09 -0800

    added f6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,14 @@
 void print_line(int n)
 {
 int j;
-for (j = 0; j <= n; j++)
-{
 if (n <= 0)
 {
 _putchar('\n');
+return;
 }
-else
+for (j = 0; j < n; j++)
 {
 _putchar('-');
 }
-}
 _putchar('\n');
 }
-
