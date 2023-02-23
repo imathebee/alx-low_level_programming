95b21419902e0f9cf344efb043be8150402e471d
Imane O <ouiyzmeimane@gmail.com>
2023-02-23 08:21:53 -0800

    ididit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 #include "main.h"
 /**
@@ -9,8 +7,6 @@
  */
 void positive_or_negative(int i)
 {
-srand(time(0));
-i = rand() - RAND_MAX / 2;
 if  (i == 0)
 {
 printf("%d is zero\n", i);
