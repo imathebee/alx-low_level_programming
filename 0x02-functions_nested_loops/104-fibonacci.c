033556adb15811920b5bc510d4f908bef949d5c7
Imane O <ouiyzmeimane@gmail.com>
2023-02-22 18:50:33 -0800

    added

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,12 +7,25 @@
  */
 int main(void)
 {
+int n;
 int i;
 int num1 = 1;
-int num2 = 2;
+int num2 = 1;
 int fib;
+printf("Enter the number of terms: ");
+scanf("%d", &n);
+if (n == 1)
+{
+printf("%d\n", num1);
+}
+else if (n == 2)
+{
+printf("%d, %d\n", num1, num2);
+}
+else
+{
 printf("%d, %d", num1, num2);
-for (i = 2; i < 98; i++)
+for (i = 3; i <= n; i++)
 {
 fib = num1 + num2;
 printf(", %d", fib);
@@ -20,5 +33,6 @@ num1 = num2;
 num2 = fib;
 }
 printf("\n");
+}
 return (0);
 }
