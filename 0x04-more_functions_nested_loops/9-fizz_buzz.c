7cc4673c986646ecaa8535f92d7989be09289d9e
Imane O <ouiyzmeimane@gmail.com>
2023-02-23 17:08:09 -0800

    addedf9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-int i;  
+int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-{       
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else if (i % 3 == 0)
+{
 printf("Fizz");
 printf(" ");
 }
-if (i % 5 == 0)
-{       
-printf("Buzz");
-printf(" ");
-}       
-if ((i % 3) == 0 && (i % 5) == 0)
+else if (i % 5 == 0)
 {
-printf("FizzBuzz");
+printf("Buzz");
 printf(" ");
 }
 else
 {
 printf("%d", i);
-printf (" ");
+printf(" ");
 }
 }
 printf("\n");
