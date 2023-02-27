25f93d17867dfd276b883cdfd99595b8cd8d5383
Imane O <ouiyzmeimane@gmail.com>
2023-02-27 13:12:21 -0800

    added f4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout in reverse
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *str)
+{
+int i;
+int len;
+len= 0;
+while (str[len] != '\0')
+{
+len++;
+}
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
