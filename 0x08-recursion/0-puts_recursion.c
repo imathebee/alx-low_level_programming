23d88471b6afdc78e33aa42bab622da6ad92a1b6
Imane O <ouiyzmeimane@gmail.com>
2023-03-08 11:27:49 -0800

    added f0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _puts_recursion - prints a string to the console recursively
+ *
+ * @s: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+
