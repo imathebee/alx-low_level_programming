db0da0c8ed7272e0364612d9f9ce0f691d8570f8
Imane O <ouiyzmeimane@gmail.com>
2023-03-10 12:51:27 -0800

    added f0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints the name of the program
+ * @argc: Number of arguments passed
+ * @argv: Array of pointers to passed arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
