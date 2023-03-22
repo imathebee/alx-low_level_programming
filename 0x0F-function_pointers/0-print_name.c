9da599f46fe8fb5b797b6e202c67bf07a6230458
Imane O <ouiyzmeimane@gmail.com>
2023-03-22 15:02:49 -0700

    added f0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_name - Calls a function pointer with a name argument
+ * @name: Pointer to a string representing the name to be printed
+ * @f: Pointer to a function that takes
+ * a char pointer as its argument and returns void
+ *
+ * Description: This function takes a string representing
+ * a name and a function pointer as arguments.
+ * The function pointer is called with the name as its argument.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
