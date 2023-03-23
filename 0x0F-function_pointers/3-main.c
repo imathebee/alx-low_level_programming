d85931d98388bac7d510e48f733a81604fe540bb
Imane O <ouiyzmeimane@gmail.com>
2023-03-22 18:29:27 -0700

    second attempt

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,24 +9,28 @@
  *
  * Return: Always 0 (success)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int (*op_func)(int, int);
+int num1, num2;
+char *operator;
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op_func = get_op_func(argv[2]);
-if (op_func == NULL)
+num1 = atoi(argv[1]); /*if argument is string convert to number*/
+num2 = atoi(argv[3]); /* as stated above */
+operator = argv[2];
+if (get_op_func(operator) == NULL || operator[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-result = op_func(num1, num2);
-printf("%d\n", result);
+if ((*operator == 47 || *operator == 37) && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(operator)(num1, num2));
 return (0);
 }
