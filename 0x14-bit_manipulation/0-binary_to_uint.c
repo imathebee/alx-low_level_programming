67b29a8bd4e3c8df908b8445f0c958049744fb00
Imane O <ouiyzmeimane@gmail.com>
2023-04-08 20:39:20 -0700

    trying

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int len;
 int i;
 unsigned int result = 0;
-len = strlen(b);
 if (b == NULL)
 {
 return (0);
 }
-for (i = 0; i < len; i++)
+for (i = 0; b[i] != '\0'; i++)
 {
 if (b[i] != '0' && b[i] != '1')
-{
-return (0);
-}
-if (result > UINT_MAX / 2)
 {
 return (0);
 }
